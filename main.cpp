cfd28df85bd86b46bd836cc75d639ea553371506
Megan <mko5258@psu.edu>
2023-10-05 01:33:24 -0400

    Changed .cpp file

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+
+
+using namespace std;
+
+// AERSP 424 Homework 1: Question 3
+// Goal: Write a program to predict roll rate of aircraft
+// for up to five seconds and print the result at every time step
